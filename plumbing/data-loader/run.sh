b7e730f712a9effe280eed6561f1c21f0589a4eb
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-09-30 13:30:05 +0000

    Trying to get the autoloading running. Hard

--- plumbing/data-loader/run.sh
+++ plumbing/data-loader/run.sh
@@ -1,9 +1,13 @@
-mb heart
-mb cities
-mb iris
-cp data/heart.csv heart/heart.csv
-cp data/iris.csv iris/iris.csv
-cp data/cities.csv cities/cities.csv
+#!/bin/sh
+
+#mc alias set minio $MINIO_SERVER_HOST $MINIO_SERVER_ACCESS_KEY $MINIO_SERVER_SECRET_KEY
+
+mc mb heart
+mc mb cities
+mc mb iris
+mc cp ./data/heart.csv heart/heart.csv
+mc cp ./data/iris.csv iris/iris.csv
+mc cp ./data/cities.csv cities/cities.csv
 
 
 
