a93767727da00ee44634e60f220ac4c3dadc021d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-10-01 19:09:45 +0000

    Fix the script, I was doing it wrong to add files

--- plumbing/data-loader/run.sh
+++ plumbing/data-loader/run.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-#mc alias set minio $MINIO_SERVER_HOST $MINIO_SERVER_ACCESS_KEY $MINIO_SERVER_SECRET_KEY
+mc alias set minio http://$MINIO_SERVER_HOST:9000 $MINIO_SERVER_ACCESS_KEY $MINIO_SERVER_SECRET_KEY
 
-mc mb heart
-mc mb cities
-mc mb iris
-mc cp ./data/heart.csv heart/heart.csv
-mc cp ./data/iris.csv iris/iris.csv
-mc cp ./data/cities.csv cities/cities.csv
-
-
-
-#docker run --rm -it --name minio-client     --env MINIO_SERVER_HOST="minio"     --env MINIO_SERVER_ACCESS_KEY="minio-root-user"  --network spark-playground_spark    --env MINIO_SERVER_SECRET_KEY="minio-root-password" bitnami/minio-client  --  mb supreme
-
+mc mb minio/heart
+mc mb minio/cities
+mc mb minio/iris
+mc cp ./data/heart.csv minio/heart/heart.csv
+mc cp ./data/iris.csv minio/iris/iris.csv
+mc cp ./data/cities.csv minio/cities/cities.csv
